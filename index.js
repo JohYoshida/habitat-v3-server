88c81efffb6ed4c304b55c051cf16599d8def84b
Joh Yoshida <johyoshida@gmail.com>
2020-04-29 19:06:17 -0700

    Order workouts by descending creation date

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ app.get("/workouts/:exercise_id", (req, res) => {
   // Get workouts
   knex("workouts")
     .where({ exercise_id })
+    .orderBy("createdAt", "desc")
     .then(workouts => {
       res.send({ msg: "Get workouts", data: workouts });
     })
