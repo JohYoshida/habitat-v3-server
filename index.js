9c8ad8d86c4c3a5a36fd0a67c5e805d09b20ee8c
Joh Yoshida <johyoshida@gmail.com>
2020-04-30 12:31:42 -0700

    Added tracking logs

--- index.js
+++ index.js
@@ -147,6 +147,9 @@ app.post("/workout", (req, res) => {
           } else if (exercise.mode === "time") {
             amount = seconds;
           }
+          console.log("amount", typeof(amount), amount);
+          console.log("lifetimeTotal", typeof(exercise.lifetimeTotal), exercise.lifetimeTotal);
+          console.log(exercise.lifetimeTotal + amount);
           knex("exercises")
             .first()
             .where({ id: exercise_id })
