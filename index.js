2ae64a0851525cf9d5e895b8f84b73ec77901747
Joh Yoshida <johyoshida@gmail.com>
2020-05-21 14:37:14 -0700

    Prettier

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const moment = require("moment");
-const { v1: uuid } = require("uuid");
+const {v1: uuid} = require("uuid");
 const PORT = process.env.PORT || 4000;
 const app = express();
 
@@ -23,39 +23,39 @@ app.get("/", (req, res) => {
 app.get("/exercises", (req, res) => {
   // Get exercises
   knex("exercises")
-  .orderBy("name")
+    .orderBy("name")
     .then(exercises => {
-      res.send({ msg: "Get exercises", data: exercises });
+      res.send({msg: "Get exercises", data: exercises});
     })
     .catch(err => {
-      res.send({ msg: "Failed to get exercises." });
+      res.send({msg: "Failed to get exercises."});
       console.log("Error!", err);
     });
 });
 
 // Get a particular exercise by id
 app.get("/exercise/:id", (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   // Get exercises
   knex("exercises")
     .first()
-    .where({ id })
+    .where({id})
     .then(exercise => {
-      res.send({ msg: "Get exercise", data: exercise });
+      res.send({msg: "Get exercise", data: exercise});
     })
     .catch(err => {
-      res.send({ msg: "Failed to get exercise.", id });
+      res.send({msg: "Failed to get exercise.", id});
       console.log("Error!", err);
     });
 });
 
 // Add an exercise to database
 app.post("/exercise", (req, res) => {
-  const { name, mode } = req.body;
+  const {name, mode} = req.body;
   // Check for existing exercise
   knex("exercises")
     .first()
-    .where({ name })
+    .where({name})
     .then(exercise => {
       if (exercise) {
         res.send({
@@ -66,49 +66,49 @@ app.post("/exercise", (req, res) => {
         // Add to database
         const id = uuid();
         knex("exercises")
-          .insert({ id, name, mode, lifetimeTotal: 0 })
+          .insert({id, name, mode, lifetimeTotal: 0})
           .then(() => {
-            res.send({ msg: "Registered exercise", name, id });
+            res.send({msg: "Registered exercise", name, id});
           })
           .catch(err => {
-            res.send({ msg: "Failed to register exercise." });
+            res.send({msg: "Failed to register exercise."});
             console.log("Error!", err);
           });
       }
     })
     // TODO: verify what this catch does
     .catch(err => {
-      res.send({ msg: "Failed duplicate exercise check." });
+      res.send({msg: "Failed duplicate exercise check."});
       console.log("Error!", err);
     });
 });
 
 // Add a list of exercises to database
 app.post("/exercises", (req, res) => {
-  const { list } = req.body;
+  const {list} = req.body;
   list.forEach(exercise => {
-    let { id, name, mode, lifetimeTotal } = exercise;
+    let {id, name, mode, lifetimeTotal} = exercise;
     knex("exercises")
-      .insert({ id, name, mode, lifetimeTotal })
+      .insert({id, name, mode, lifetimeTotal})
       .then(() => {
-        console.log({ msg: "Registered exercise", name, id });
+        console.log({msg: "Registered exercise", name, id});
       })
       .catch(err => {
         console.log("Error! Failed to register exercise.", err);
       });
-  })
-  res.send({ msg: "Registered all exercises."});
-})
+  });
+  res.send({msg: "Registered all exercises."});
+});
 
 // Delete an exercise by id
 app.delete("/exercise", (req, res) => {
-  const { id, name } = req.body;
+  const {id, name} = req.body;
   knex("exercises")
-    .where({ id })
+    .where({id})
     .del()
     .then(() => {
       knex("workouts")
-        .where({ exercise_id: id })
+        .where({exercise_id: id})
         .del()
         .then(() => {
           res.send({
@@ -125,38 +125,38 @@ app.delete("/exercise", (req, res) => {
         });
     })
     .catch(err => {
-      res.send({ msg: "Failed to delete exercise." });
+      res.send({msg: "Failed to delete exercise."});
       console.log("Error!", err);
     });
 });
 
 // Get all workouts associated with an exercise by exercise_id
 app.get("/workouts/:exercise_id", (req, res) => {
-  const { exercise_id } = req.params;
+  const {exercise_id} = req.params;
   // Get workouts
   knex("workouts")
-    .where({ exercise_id })
+    .where({exercise_id})
     .orderBy("createdAt", "asc")
     .then(workouts => {
-      res.send({ msg: "Get workouts", data: workouts });
+      res.send({msg: "Get workouts", data: workouts});
     })
     .catch(err => {
-      res.send({ msg: "Failed to get workouts." });
+      res.send({msg: "Failed to get workouts."});
       console.log("Error!", err);
     });
 });
 
 // Add a workout to database
 app.post("/workout", (req, res) => {
-  const { exercise_id, reps, sets, seconds } = req.body;
+  const {exercise_id, reps, sets, seconds} = req.body;
   const id = uuid();
   const createdAt = moment().format();
   knex("workouts")
-    .insert({ id, exercise_id, reps, sets, seconds, createdAt })
+    .insert({id, exercise_id, reps, sets, seconds, createdAt})
     .then(() => {
       knex("exercises")
         .first()
-        .where({ id: exercise_id })
+        .where({id: exercise_id})
         .then(exercise => {
           let amount;
           if (exercise.mode === "reps and sets") {
@@ -166,12 +166,12 @@ app.post("/workout", (req, res) => {
           }
           knex("exercises")
             .first()
-            .where({ id: exercise_id })
+            .where({id: exercise_id})
             .update({
               lifetimeTotal: Number(exercise.lifetimeTotal) + amount
             })
             .then(() => {
-              res.send({ msg: "Registered workout", id });
+              res.send({msg: "Registered workout", id});
             })
             .catch(err => {
               let msg =
@@ -192,40 +192,40 @@ app.post("/workout", (req, res) => {
     })
     .catch(err => {
       let msg = "Failed to register workout.";
-      res.send({ msg });
+      res.send({msg});
       console.log("Error!", msg, id, err);
     });
 });
 
 // Add a list of workouts to database
 app.post("/workouts", (req, res) => {
-  const { list } = req.body;
+  const {list} = req.body;
   list.forEach(workout => {
-    const { id, exercise_id, reps, sets, seconds, createdAt } = workout;
+    const {id, exercise_id, reps, sets, seconds, createdAt} = workout;
     knex("workouts")
-      .insert({ id, exercise_id, reps, sets, seconds, createdAt })
+      .insert({id, exercise_id, reps, sets, seconds, createdAt})
       .then(() => {
-        console.log({ msg: "Registered workout", id });
+        console.log({msg: "Registered workout", id});
       })
       .catch(err => {
         console.log("Error! Failed to register workout.", err);
       });
-  })
-  res.send({ msg: "Registered all workouts."});
+  });
+  res.send({msg: "Registered all workouts."});
 });
 
 // Delete a workout by id and decrease its associated exercise's lifetimeTotal
 app.delete("/workout", (req, res) => {
-  const { id, exercise_id, amount } = req.body;
+  const {id, exercise_id, amount} = req.body;
   // Get the exercise for its current lifetimeTotal
   knex("exercises")
     .first()
-    .where({ id: exercise_id })
+    .where({id: exercise_id})
     .then(exercise => {
       // Set the new lifetimeTotal
       knex("exercises")
         .first()
-        .where({ id: exercise_id })
+        .where({id: exercise_id})
         .update({
           lifetimeTotal: exercise.lifetimeTotal - amount
         })
@@ -233,26 +233,26 @@ app.delete("/workout", (req, res) => {
           // Delete the workout
           knex("workouts")
             .first()
-            .where({ id })
+            .where({id})
             .del()
             .then(() => {
-              res.send({ msg: "Deleted workout", id });
+              res.send({msg: "Deleted workout", id});
             })
             .catch(err => {
               let msg = "Failed to delete workout.";
-              res.send({ msg });
+              res.send({msg});
               console.log("Error!", msg, id, err);
             });
         })
         .catch(err => {
           let msg = "Failed to decrease exercise's lifetimeTotal.";
-          res.send({ msg });
+          res.send({msg});
           console.log("Error!", msg, exercise_id, err);
         });
     })
     .catch(err => {
       let msg = "Failed to retrieve exercise.";
-      res.send({ msg });
+      res.send({msg});
       console.log("Error!", msg, exercise_id, err);
     });
 });
