f1d2c5b93f31f7f1d4de19f4d2c118087e97619d
Joh Yoshida <johyoshida@gmail.com>
2020-12-22 15:06:47 -0800

    Added migration for goals table

---
+++ db/migrations/20201222145503_goals.js.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.dropTableIfExists("goals"),
+    knex.schema.createTable("goals", table => {
+      table.uuid("id").primary();
+      table.uuid("exercise_id");
+      table.string("type"); // daily, weekly, monthly, yearly
+      table.integer("goal");
+      table.boolean("complete");
+      table.string("createdAt");
+    })
+  ]);
+};
+
+exports.down = function(knex) {
+  return Promise.all([knex.schema.dropTableIfExists("goals")]);
+};
