32272d163f2e8842af86bc1498510bb66d615da6
Joh Yoshida <johyoshida@gmail.com>
2020-12-27 17:36:13 -0800

    Bugfix

--- routes/goals.js
+++ routes/goals.js
@@ -1,5 +1,5 @@
 // Database requirements
-const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
+const dbconfig = require("../knexfile.js")[process.env.DB_ENV];
 const knex = require("knex")(dbconfig);
 
 // Get all goals
