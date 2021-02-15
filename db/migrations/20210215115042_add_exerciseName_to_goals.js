01b4589388f92c563696d853d98227afb22c6307
Joh Yoshida <johyoshida@gmail.com>
2021-02-15 11:56:30 -0800

    Add exercise_name column to goals table

---
+++ db/migrations/20210215115042_add_exerciseName_to_goals.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.table("goals", table => {
+      table.integer("exercise_name");
+    })
+  ]);
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.table("goals", table => {
+      table.dropColumn("exercise_name");
+    })
+  ]);
+};
