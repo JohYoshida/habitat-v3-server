fcdad9a353131c3a2c604558881fd41f340be357
Joh Yoshida <johyoshida@gmail.com>
2021-03-12 16:37:43 -0800

    Use SSL

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,8 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      port: process.env.DB_PORT,
+      ssl: true
     },
     migrations: {
       directory: "./db/migrations",
@@ -42,7 +43,8 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      port: process.env.DB_PORT,
+      ssl: true
     },
     pool: {
       min: 2,
